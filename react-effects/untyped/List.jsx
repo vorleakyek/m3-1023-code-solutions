d9e290c95f275ef7013fe2ac737701963c582421
Vorleak Yek <vorleakyek@gmail.com>
2023-11-13 05:44:38 +0000

    Complete react-effects exercise.

---
+++ react-effects/untyped/List.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import { readItems } from './read';
+
+export function List() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState();
+
+  // Your code here:
+  //  - When the component mounts:
+  //    - Read the items using `readItems` and update state so the list displays
+  //    - Handle errors from `readItems`
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.id}: {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
