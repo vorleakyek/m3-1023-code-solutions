7614288d74187d4467b12bbed25bdbb29f40fdf0
Vorleak Yek <vorleakyek@gmail.com>
2023-11-19 07:10:05 +0000

    Complete react-carousel-component challenge.

---
+++ react-carousel-component/my-app/src/data.tsx
@@ -0,0 +1,32 @@
+export type imageObject = {
+  src: string;
+  alt: string;
+};
+
+
+export const imagesData: imageObject[] = [
+  {
+    src: '/images/fushiguro.webp',
+    alt: 'Megumi Fushiguro',
+  },
+  {
+    src: '/images/inumaki.webp',
+    alt: 'Toge Inumaki',
+  },
+  {
+    src: '/images/itadori.webp',
+    alt: 'Yuji Itadori',
+  },
+  {
+    src: '/images/kugisaki.webp',
+    alt: 'Nobara Kugisaki',
+  },
+  {
+    src: '/images/panda.webp',
+    alt: 'Panda',
+  },
+  {
+    src: '/images/zen-in.webp',
+    alt: "Maki Zen'in",
+  },
+];
