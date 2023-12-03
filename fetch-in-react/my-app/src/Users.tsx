e1b8322f9f6317ccac766c3017d5a62fa0b90a28
Vorleak Yek <vorleakyek@gmail.com>
2023-12-03 06:50:00 +0000

    Complete fetch-in-react exercise.

---
+++ fetch-in-react/my-app/src/Users.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { UserDetails } from './UserDetails';
+import { UserCard } from './UserCard';
+import './Users.css';
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  company: {
+    name: string;
+  };
+};
+export function Users() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>();
+
+  /* your code here (hint: useEffect) */
+  useEffect(()=>{
+
+    async function startFetching() {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+          throw new Error(`server status code: ${response.status}`);
+        }
+        const result = await response.json();
+        setUsers(result);
+      } catch (err) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    startFetching();
+  },[]);
+
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    console.error('Fetch error:', error);
+    return (
+      <p>Error! {error instanceof Error ? error.message : 'Unknown Error'}</p>
+    );
+  }
+  return (
+    <div className="container">
+      <UserList users={users} onClick={(user) => setUser(user)} />
+      {user && (
+        <UserDetails userId={user.id} onCancel={() => setUser(undefined)} />
+      )}
+    </div>
+  );
+}
+
+type ListProps = {
+  users: User[];
+  onClick: (user: User) => void;
+};
+function UserList({ users, onClick }: ListProps) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <UserCard user={user} onClick={onClick} />
+        </li>
+      ))}
+    </ul>
+  );
+}
